a07f3e7ea9b48bb9d344e5d0b5d76f1c26215d10
zm235 <zm235@cornell.edu>
2021-03-13 15:43:03 -0500

    FindById in mall_category by Mybatis

---
+++ src/main/java/com/imooc/mall/dao/CategoryMapper.java
@@ -0,0 +1,13 @@
+package com.imooc.mall.dao;
+
+import com.imooc.mall.pojo.Category;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+//@Mapper
+public interface CategoryMapper {
+
+    @Select("Select * from mall_category where id = #{id}")
+    Category findById(@Param("id") Integer id);
+
+}
--- src/test/java/com/imooc/mall/MallApplicationTests.java
+++ src/test/java/com/imooc/mall/MallApplicationTests.java
@@ -1,7 +1,10 @@
 package com.imooc.mall;
 
+import com.imooc.mall.dao.CategoryMapper;
+import com.imooc.mall.pojo.Category;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 class MallApplicationTests {
 
+    @Autowired
+    private CategoryMapper categoryMapper;
+
     @Test
     void contextLoads() {
+        Category category = categoryMapper.findById(100001);
+        System.out.println(category.toString());
     }
 
 }
